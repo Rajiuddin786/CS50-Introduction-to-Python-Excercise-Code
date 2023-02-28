453cd9fd048373451f58ac75c2b9c284ea906d1b
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-02-28 05:48:53 +0000

    Tue, Feb 28, 2023, 5:48 AM +00:00

--- bank.py
+++ bank.py
@@ -16,15 +16,15 @@ def element_pop(p):
 
     for i in range(len(list_hello)):
           if(list_hello[0]=='H'):
-               r+=1
+               r=r+1
           elif(list_hello[1]=='e'):
-               r+=1
+               r=r+1
           elif(list_hello[2]=='l'):
-               r+=1
+               r==r+1
           elif(list_hello[3]=='l'):
-               r+=1
+               r=r+1
           elif(list_hello[4]=='o'):
-               r+=1
+               r=r+1
           else:
                break
 
