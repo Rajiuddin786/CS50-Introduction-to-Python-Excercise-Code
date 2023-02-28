f5ee21fba9c2042598a8d63516387fd4571ec166
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-02-28 05:34:13 +0000

    Tue, Feb 28, 2023, 5:34 AM +00:00

--- check.py
+++ check.py
@@ -1,11 +1,12 @@
 def element_pop(p):
     r=0
+    list_hello=[]
     length=len(p)#Finding the length of the string
     #string to list
     for i in p:
-          list_hello[10]=i
+         list_hello.append(i)
 
-    """if(list_hello[0]=='H'):
+    if(list_hello[0]=='H'):
             r+=1
     elif(list_hello[1]=='e'):
               r+=1
@@ -14,7 +15,7 @@ def element_pop(p):
     elif(list_hello[3]=='l'):
              r+=1
     elif(list_hello[4]=='o'):
-            r+=1"""
-    print(list_hello)
+            r+=1
+    print(r)
 
 element_pop('hello')
