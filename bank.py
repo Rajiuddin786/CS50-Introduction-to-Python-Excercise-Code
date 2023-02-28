8e1baad0b14d2834720fb45fe6a9cb4fe8ada6c8
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-02-28 05:38:48 +0000

    Tue, Feb 28, 2023, 5:38 AM +00:00

--- bank.py
+++ bank.py
@@ -10,20 +10,22 @@ def main():
 
 def element_pop(p):
     r=0
+    list_hello=[]
     length=len(p)#Finding the length of the string
     #string to list
     for i in p:
-          list_hello=i
+         list_hello.append(i)
 
-    if(list_hello[0]=='H'):
-            r+=1
-    elif(list_hello[1]=='e'):
+    for i in range(len(list_hello)):
+         if(list_hello[0]=='H'):
               r+=1
-    elif(list_hello[2]=='l'):
+         elif(list_hello[1]=='e'):
               r+=1
-    elif(list_hello[3]=='l'):
-             r+=1
-    elif(list_hello[4]=='o'):
+         elif(list_hello[2]=='l'):
+              r+=1
+         elif(list_hello[3]=='l'):
+              r+=1
+         elif(list_hello[4]=='o'):
               r+=1
 
     if(r==5):
