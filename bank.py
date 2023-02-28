9e1a5919bdcf01c944243aca7039bf8c458a7294
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-02-28 04:59:58 +0000

    Tue, Feb 28, 2023, 4:59 AM +00:00

--- bank.py
+++ bank.py
@@ -9,9 +9,9 @@ def main():
         print("100$")
 
 def element_pop(p):
-    r=0
-    length=len(p)#Finding the length of the string
-    list_hello=list(p)#string to list
+    l=p
+    length=len(l)#Finding the length of the string
+    list_hello=list(l)#string to list
     for i in range(length-1):
         element=list_hello.pop(i)
         check_hello(element)
@@ -21,15 +21,21 @@ def check_hello(collect):
     r=0
     if(collect=='H'):
         r+=1
-        
+        element_pop()
     elif(collect=='e'):
         r+=1
+        element_pop()
     elif(collect=='l'):
         r+=1
+        element_pop()
     elif(collect=='l'):
         r+=1
+        element_pop()
     elif(collect=='o'):
         r+=1
+        element_pop()
+    else
+        element_pop()
 
 
-
+main()
