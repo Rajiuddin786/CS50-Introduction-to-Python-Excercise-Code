d956a49cc86d2100aec5b1990962abc22efd195d
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-02-28 05:27:18 +0000

    Tue, Feb 28, 2023, 5:27 AM +00:00

--- check.py
+++ check.py
@@ -1,3 +1,20 @@
-h=input()
-o=h.strip().title()
-print(o)
+def element_pop(p):
+    r=0
+    length=len(p)#Finding the length of the string
+    #string to list
+    for i in p:
+          list_hello=i
+
+    """if(list_hello[0]=='H'):
+            r+=1
+    elif(list_hello[1]=='e'):
+              r+=1
+    elif(list_hello[2]=='l'):
+              r+=1
+    elif(list_hello[3]=='l'):
+             r+=1
+    elif(list_hello[4]=='o'):
+            r+=1"""
+    print(list_hello)
+
+element_pop(hello)
