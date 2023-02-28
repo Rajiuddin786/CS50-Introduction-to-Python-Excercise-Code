c0e2a1de758a5ae4e39eb5e1497f083aab0c2eef
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-02-28 04:06:56 +0000

    Tue, Feb 28, 2023, 4:06 AM +00:00

--- bank.py
+++ bank.py
@@ -1,2 +1,21 @@
 def main():
-    
+    h=input("Greeting: ").strip().title()
+
+    if(h=='Hello'):
+        print("0$")
+    elif()
+
+
+
+
+def pop(p):
+    r=0
+    length=p.len()
+    for i in range(p):
+        element=[p.pop(i)]
+
+    while True:
+        if(element[r]==H):
+            if(element[r+1]!=e):
+                if(element[r+2]!=l):
+                    if()
