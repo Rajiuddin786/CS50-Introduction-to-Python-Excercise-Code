83a1faf79dc7e13f198df487e1a9e27e326de83f
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-02-28 06:03:05 +0000

    Tue, Feb 28, 2023, 6:03 AM +00:00

--- bank.py
+++ bank.py
@@ -2,36 +2,39 @@ def main():
     my_input=input("Greeting: ").strip().title()
     check=element_pop(my_input)
     if(check==1):
-        print("0$")
+        print("100$")
     elif(check==0):
         print("20$")
     else:
-        print("100$")
+        print("0$")
 
 def element_pop(p):
     r=0
+    h=0
     list_hello=[]
     for i in p:
          list_hello.append(i)
 
     for i in range(len(list_hello)):
-          if(list_hello[0]!='H'):
-               r=r+1
-          elif(list_hello[1]!='e'):
-               r=r+1
-          elif(list_hello[2]!='l'):
-               r==r+1
-          elif(list_hello[3]!='l'):
-               r=r+1
-          elif(list_hello[4]!='o'):
-               r=r+1
-          els
-               break
+        if(list_hello[0]=='H'):
+            h=1
+        if(list_hello[0]!='H'):
+            r=r+1
+        elif(list_hello[1]!='e'):
+            r=r+1
+        elif(list_hello[2]!='l'):
+            r==r+1
+        elif(list_hello[3]!='l'):
+            r=r+1
+        elif(list_hello[4]!='o'):
+            r=r+1
+        else:
+            break
     if(r==5):
         return 1
-    elif(r<5):
+    if(r<5 and h==1):
         return 0
-    else:
+    if(r==0 and h==0):
         return -1
 
 
