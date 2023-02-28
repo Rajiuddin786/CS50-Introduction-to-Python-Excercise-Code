8498a7f4b9126fbb52bf8d8a3ac1f0980502b0cd
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-02-28 03:43:09 +0000

    Tue, Feb 28, 2023, 3:43 AM +00:00

---
+++ deep.py
@@ -0,0 +1,5 @@
+y=input("What is the answer to the Great Question of Life, the Universe and Everything ").strip().title()
+if y=="42" or y=="Fourty-Two" or y=="Forty Two" or y=="forty-two" :
+    print("Yes")
+else:
+    print("No")
