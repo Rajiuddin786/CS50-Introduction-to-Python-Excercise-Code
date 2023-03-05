da39fb0d0a8d5b7da0a75d5a3c58fb87d09e46fa
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-03-05 12:21:43 +0000

    Sun, Mar 5, 2023, 4:21 AM -08:00

---
+++ interpreter.py
@@ -0,0 +1,37 @@
+exp = input('Expression: ').strip()
+y = 0
+s = ''
+for i in exp:
+    if i == '+' or i == '-' or i == '*' or i == '/' or i == '%':
+        s = i
+        break
+
+for j in exp:
+    if j == '+' or j == '-' or j == '*' or j == '/' or j == '%':
+        continue
+    else:
+        if s == '+':
+            y = y + float(j)
+        elif s == '-':
+            y = y - float(j)
+        elif s == '*':
+            y = 1
+            y = y * float(j)
+        elif s == '/':
+            if y == 0:
+                y = 1
+                y = y * float(j)
+                continue
+            y = y / float(j)
+
+        elif s == '%':
+            if y == 0:
+                y = 1
+                y = y * float(j)
+                continue
+            y = y % float(j)
+        else:
+            print('Invalid Expression')
+            break
+
+print(y, end='')
