77ec6c0befa8f51b946eedde7650a1c9c41e3c51
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-03-07 14:36:06 +0000

    Tue, Mar 7, 2023, 6:36 AM -08:00

--- camel.py
+++ camel.py
@@ -2,19 +2,17 @@ name = input("camelCase: ").strip()
 h = ''
 s = name
 v = ''
+c = 0
 for k in name:
     if k.isupper():
         h = k
         p, m = s.split(h)
         v = p + '_' + h.lower() + m
         s = v
-
-for o in name:
-    if o.islower():
-        print(name,end='')
-        break
     else:
-        print('snake_case: ',v)
-        break
-    
+        c = 1
 
+if c == 1:
+    print(name,end='')
+else:
+    print('snake_case: ',v)
