42cd64622ebba31c71bc1c0a703526a2deb41430
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-03-10 12:28:13 +0000

    Fri, Mar 10, 2023, 4:28 AM -08:00

--- twitter.py
+++
@@ -1,7 +0,0 @@
-name = input('Input: ')
-s = ''
-for i in name:
-    if i != 'a' and i != 'e' and i != 'i' and i != 'o' and i != 'u' and i != 'A' and 'E' and i != 'O' and i != 'I' and i != 'U':
-        s = s + i
-
-print('Output:', s)
