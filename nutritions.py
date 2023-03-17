a2a632712efc4ca445efa5548104e3f44a24abaf
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-03-17 13:57:50 +0000

    Fri, Mar 17, 2023, 6:57 AM -07:00

---
+++ nutritions.py
@@ -0,0 +1,7 @@
+fruit_input = input('Item: ').strip().capitalize()
+fruit = {'Apple': 130, 'Avocado': 50, 'Banana': 110, 'Cantalope': 50, 'Grapes': 90, 'Grapefruit': 60,
+         'Honeydew Melon': 50, 'Kiwifruit': 90, 'Lemon': 15, 'Lime': 20, 'Nectarine': 60, 'Orange': 80, 'Peach': 60,
+         'Pear': 100, 'Pineapple': 70, 'Plumbs': 50, 'Sweet Cherries': 100, 'Tangerine': 50, 'Watermelon': 80}
+for i in fruit:
+    if i == fruit_input:
+        print('Calories:', fruit[i])
