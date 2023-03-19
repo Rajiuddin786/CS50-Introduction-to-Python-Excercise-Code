d78025dcafab1168627f1e324df5e26e2973ef53
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-03-19 03:51:18 +0000

    Sat, Mar 18, 2023, 8:51 PM -07:00

--- taqueria.py
+++ taqueria.py
@@ -1,5 +1,5 @@
-def main():
-    food_menu = {
+
+food_menu = {
         "Baja Taco": 4.00,
         "Burrito": 7.50,
         "Bowl": 8.50,
@@ -9,18 +9,17 @@ def main():
         "Super Quesadilla": 9.50,
         "Taco": 3.00,
         "Tortilla Salad": 8.00}
-    total = 0
-    while True:
-        food = input("Item: ").title()
-        h = ''
-        for i in food_menu:
-            if food == i:
-                total = total + food_menu[i]
-                print('Total:', total)
-                h = i
-                break
-        if food == '' or h == '':
+total = 0
+while True:
+    food = input("Item: ").title()
+    h = ''
+    for i in food_menu:
+        if food == i:
+            total = total + food_menu[i]
+            print('Total:', total)
+            h = i
             break
+    if food == '' or h == '':
+        break
 
 
-main()
