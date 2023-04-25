26d8664dd60b841e90a859668e7853ec199f12e4
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-04-25 12:36:23 +0000

    Tue, Apr 25, 2023, 5:36 AM -07:00

---
+++ grocery.py
@@ -0,0 +1,34 @@
+def main():
+    w = 0
+    g = []
+    c = 0
+    pos = 0
+    try:
+        while True:
+            i = input().strip().upper()
+            g = g + [i]
+    except EOFError:
+        a = len(g)
+        for j in g:
+            if j != ' ':
+                for u in g:
+                    if j == u:
+                        c = c + 1
+                        g[pos] = ' '
+                    pos += 1
+
+            if c != 0:
+                print(f"{c}{j}")
+            c = 0
+            pos = 0
+            for q in range(a):
+
+                if g[q] == ' ':
+                    w += 1
+            if w != a:
+                w = 0
+            else:
+                break
+
+
+main()
