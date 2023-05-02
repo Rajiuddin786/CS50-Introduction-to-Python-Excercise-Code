22f031f0fb243770f1c71c2bf918cd730bc435e9
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-05-02 13:43:00 +0000

    Tue, May 2, 2023, 6:43 AM -07:00

--- outdated.py
+++ outdated.py
@@ -1,47 +1,55 @@
-date = input('Date: ').strip()
-fix = ''
-q = ''
-try:
-    month = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"]
-
-    for t in date:
-        if t != ',':
-            fix += t
-    a_1 = fix.split(' ')
-    mon = month.index(a_1[0])
-    if mon+1 < 10:
-        o = '0' + str(mon+1)
-    else:
-        o = str(mon+1)
-    if int(a_1[1]) < 10:
-        q = '0' + a_1[1]
-    else:
-        q = a_1[1]
-    print(f"{a_1[2]}-{o}-{q}", end='')
+def main():
+    date = input('Date: ').strip()
+    send(date)
 
 
-except ValueError:
-    u = ''
-    t = ''
-    a_2 = date.split('/')
-    if int(a_2[0]) < 10:
-        t = '0' + a_2[0]
-    else:
-        t = a_2[0]
-    if int(a_2[1]) < 10:
-        u = '0' + a_2[1]
-    else:
-        u = a_2[1]
-    print(f"{a_2[2]}-{t}-{u}", end='')
+def send(ate):
+    fix = ''
+    try:
+        month = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"]
+
+        for t in ate:
+            if t != ',':
+                fix += t
+        a_1 = fix.split(' ')
+        mon = month.index(a_1[0])
+        if int(a_1[1]) > 30:
+            main()
+        if mon + 1 < 10:
+            o = '0' + str(mon + 1)
+        else:
+            o = str(mon + 1)
+        if int(a_1[1]) < 10:
+            q = '0' + a_1[1]
+        else:
+            q = a_1[1]
+        print(f"{a_1[2]}-{o}-{q}", end='')
+
+    except ValueError:
+        a_2 = ate.split('/')
+        if int(a_2[0]) > 31 or int(a_2[1]) > 13:
+            main()
+        if int(a_2[0]) < 10:
+            t = '0' + a_2[0]
+        else:
+            t = a_2[0]
+        if int(a_2[1]) < 10:
+            u = '0' + a_2[1]
+        else:
+            u = a_2[1]
+        print(f"{a_2[2]}-{t}-{u}", end='')
+
+
+main()
