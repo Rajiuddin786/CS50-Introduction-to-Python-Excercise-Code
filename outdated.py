3c0922c57af7fd0a737b8923df7b532a40d108d5
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-05-02 12:09:13 +0000

    Tue, May 2, 2023, 5:09 AM -07:00

---
+++ outdated.py
@@ -0,0 +1,24 @@
+date = input().strip()
+try:
+    month = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"]
+
+    a_1 = date.split(' ')
+    mon = month.index(a_1[0])
+    print(f"{a_1[2]}/{mon + 1}/{a_1[1]}")
+
+
+except ValueError:
+    a_2 = date.split('/')
+    print(f"{a_2[2]}/{a_2[1]}/{a_2[0]}")
