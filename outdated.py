3cb62ba0d1fc56ad7b9cc3d06cc3167ebefc9432
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-05-02 12:22:13 +0000

    Tue, May 2, 2023, 5:22 AM -07:00

--- outdated.py
+++ outdated.py
@@ -16,9 +16,9 @@ try:
 
     a_1 = date.split(' ')
     mon = month.index(a_1[0])
-    print(f"{a_1[2]}-0{mon + 1}-0{a_1[1]}")
+    print(f"{a_1[2]}-0{a_1[0]}-0{mon+1}",end='')
 
 
 except ValueError:
     a_2 = date.split('/')
-    print(f"{a_2[2]}-0{a_2[1]}-0{a_2[0]}",end='')
+    print(f"{a_2[2]}-0{a_2[0]}-0{a_2[1]}",end='')
