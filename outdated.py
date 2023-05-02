9f221acf639998c7237898cb595901f4113ff00a
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-05-02 12:30:04 +0000

    Tue, May 2, 2023, 5:30 AM -07:00

--- outdated.py
+++ outdated.py
@@ -1,4 +1,5 @@
 date = input('Date: ').strip()
+fix = ' '
 try:
     month = [
         "January",
@@ -14,11 +15,15 @@ try:
         "November",
         "December"]
 
-    a_1 = date.split(' ')
+    for t in date:
+        if t != ',':
+            fix+=t
+
+    a_1 = fix.split(' ')
     mon = month.index(a_1[0])
     print(f"{a_1[2]}-0{mon + 1}-0{a_1[1]}",end='')
 
 
 except ValueError:
-    a_2 = date.split('/')
+    a_2 = fix.split('/')
     print(f"{a_2[2]}-0{a_2[0]}-0{a_2[1]}",end='')
