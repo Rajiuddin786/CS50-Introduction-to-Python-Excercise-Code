6d9b9cc49493061e6960f74c1617cad47e82f31a
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-06-23 04:37:38 +0000

    Thu, Jun 22, 2023, 9:37 PM -07:00

--- figlet.py
+++ figlet.py
@@ -1,6 +1,37 @@
-from pyfiglet import figlet
-figlet = Figlet()
-fi = input("Input: ")
-figlet.getFonts()
-figlet.setFonts(font = f)
-print(figlet.renderText(s))
+import sys
+import random
+from pyfiglet import Figlet
+
+def get_random():
+    f = Figlet()
+    fonts = f.getFonts()
+    return random.choice(fonts)
+
+def font_input():
+    text = input('Input: ')
+    return text
+
+def output_font(t,fonts):
+    f = Figlet(font=fonts)
+    print(f.renderText(t))
+
+
+def main():
+    f = Figlet()
+    c = 0
+    if len(sys.argv) == 1:
+        fonts = get_random()
+    elif sys.argv[1] == '-f' or sys.argv[1] == '--font':
+        for i in f.getFonts():
+            if i == sys.argv[2]:
+                fonts = i
+                c = 1
+                break
+    if c == 0:
+        sys.exit("Not a Valid Font")
+
+    text = font_input()
+    output_font(text,fonts)
+
+
+main()
