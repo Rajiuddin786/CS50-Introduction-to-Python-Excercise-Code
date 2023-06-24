baae1e4121eb56f47051a1ddf8733689e30b7c73
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-06-24 13:38:58 +0000

    Sat, Jun 24, 2023, 6:38 AM -07:00

---
+++ adieu.py
@@ -0,0 +1,16 @@
+store = []
+try:
+    while True:
+        name = input('Name: ')
+        store = store + [name]
+
+except EOFError:
+    c = len(store)
+    for i in range(c):
+        if i == 0:
+            print('Adieu, adieu, to',store[0],end=', ')
+        elif i == (c-1):
+            print('and',store[c-1])
+
+        else:
+            print(store[i],end=', ')
