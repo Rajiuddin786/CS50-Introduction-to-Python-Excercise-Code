29f20cec8d6831401c9f9c6a8985084ab9cae0ea
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-06-24 13:47:58 +0000

    Sat, Jun 24, 2023, 6:47 AM -07:00

--- adieu.py
+++ adieu.py
@@ -1,16 +1,22 @@
 store = []
 try:
     while True:
-        name = input()
+        name = input('Name: ')
         store = store + [name]
 
 except EOFError:
     c = len(store)
     for i in range(c):
-        if i == 0:
-            print('Adieu, adieu, to',store[0],end=', ')
-        elif i == (c-1):
-            print('and',store[c-1])
+        if c != 1:
+            if i == 0:
+                print('Adieu, adieu, to',store[0],end=', ')
+            elif i == (c-1):
+                print('and',store[c-1])
+
+            else:
+                print(store[i],end=', ')
 
         else:
-            print(store[i],end=', ')
+            print('Adieu, adieu, to',store[0])
+
+
