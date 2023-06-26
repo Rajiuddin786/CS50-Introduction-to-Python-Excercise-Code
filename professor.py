b20206777e2fc8fb1fdd5fb8bbcaec9e394177b5
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-06-26 11:44:43 +0000

    Mon, Jun 26, 2023, 4:44 AM -07:00

--- professor.py
+++ professor.py
@@ -1,55 +1,57 @@
 import random
 
-def get_level():
+def take_level():
     while True:
-        level = input("Level: ")
-        if level in ['1', '2', '3']:
-            return int(level)
+        try:
+            level = int(input('Level: '))
+            if level == 0 or level == 4:
+                take_level()
+            return level
+        except ValueError:
+            take_level()
+
+
+def check_answer(a,b,an):
+    try:
+        ans = int(an)
+        if (a+b) ==  ans:
+            return 1
+        else:
+            return 0
+    except ValueError:
+        return -1
 
 def generate_integer(level):
     if level == 1:
-        return random.randint(0, 9)
+        x = random.randint(0, 9)
+        y = random.randint(0, 9)
     elif level == 2:
-        return random.randint(10, 99)
+        x = random.randint(10, 99)
+        y = random.randint(10, 99)
     elif level == 3:
-        return random.randint(100, 999)
+        x = random.randint(100, 999)
+        y = random.randint(100, 999)
     else:
-        raise ValueError("Invalid level. Level must be 1, 2, or 3.")
+        raise ValueError('Invalid Input')
+    count = 0
+    while True:
+        count+=1
+        answer = input(f'{x} + {y} = ')
+        check = check_answer(x,y,answer)
+        if check == 1:
+            return 1
+        elif check == -1 or check == 0:
+            print('EEE')
+        if count == 3:
+            print(f'{x} + {y} = ',(x+y))
+            return 0
 
-def solve_problem(problem):
-    x, y = problem
-    correct_answer = x + y
-    tries = 0
-
-    while tries < 3:
-        answer = input(f"{x} + {y} = ")
-        try:
-            answer = int(answer)
-            if answer == correct_answer:
-                return True
-            else:
-                print("EEE")
-                tries += 1
-        except ValueError:
-            print("EEE")
-            tries += 1
-
-    print(f"{x} + {y} = {correct_answer}")
-    return False
 
 def main():
-    level = get_level()
     score = 0
+    level = take_level()
+    for i in range(9):
+        score = score + generate_integer(level)
+    print('Score: ',score)
 
-    for _ in range(10):
-        x = generate_integer(level)
-        y = generate_integer(level)
-        problem = (x, y)
-
-        if solve_problem(problem):
-            score += 1
-
-    print(f"Score: {score}")
-
-if __name__ == "__main__":
-    main()
+main()
