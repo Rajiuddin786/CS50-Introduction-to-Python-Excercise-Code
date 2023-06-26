a5d980660781c173a7992afe1e7b4a9b8a4e012f
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-06-26 04:28:28 +0000

    Sun, Jun 25, 2023, 9:28 PM -07:00

---
+++ professor.py
@@ -0,0 +1,45 @@
+import random
+
+def take_level():
+    while True:
+        level = int(input('Level: '))
+        if level == 1 or level == 2 or level == 3:
+            break
+
+
+def check_answer(a,b,an):
+    try:
+        ans = int(an)
+        if (a+b) ==  ans:
+            return 1
+        else:
+            return 0
+    except ValueError:
+        return -1
+
+def calculator():
+
+    x = random.randint(1, 10)
+    y = random.randint(1, 10)
+    count = 0
+    while True:
+        count+=1
+        answer = input(f'{x} + {y} = ')
+        check = check_answer(x,y,answer)
+        if check == 1:
+            return 1
+        elif check == -1 or check == 0:
+            print('EEE')
+        if count == 3:
+            print(f'{x} + {y} = ',(x+y))
+            return 0
+
+
+def main():
+    score = 0
+    take_level()
+    for i in range(9):
+        score = score + calculator()
+    print('Score: ',score)
+
+main()
