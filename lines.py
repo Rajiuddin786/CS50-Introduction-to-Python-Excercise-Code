3bb3368507210df552dd42d51238c0661847b614
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-04 06:03:09 +0000

    Mon, Jul 3, 2023, 11:03 PM -07:00

--- lines.py
+++ lines.py
@@ -12,6 +12,6 @@ else:
     else:
         with open(sys.argv[1],'r') as file:
             for lines in file:
-                if not lines.startswith('#') and not lines.startswith('\n'):
+                if not lines.strip().startswith('#') and lines.strip():
                     count+=1
 print(count)
