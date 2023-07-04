1d9040e22214a59b639f9e42a39b1fa37c1f5ccf
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-04 05:53:12 +0000

    Mon, Jul 3, 2023, 10:53 PM -07:00

---
+++ lines.py
@@ -0,0 +1,17 @@
+import sys
+
+count = 0
+if len(sys.argv) > 2:
+    sys.exit('Too Many Argument')
+elif len(sys.argv) < 2:
+    sys.exit('Too Few Argument')
+else:
+    a = sys.argv[1].strip().endswith('.py')
+    if a == False:
+        sys.exit('Not a Python file')
+    else:
+        with open(sys.argv[1],'r') as file:
+            for lines in file:
+                if not lines.startswith('#') and not lines.startswith('\n'):
+                    count+=1
+print(count)
