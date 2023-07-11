7aa19d76e28e0b1ec5c5a240707acbc1c9d33f6e
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-11 03:41:12 +0000

    Mon, Jul 10, 2023, 8:41 PM -07:00

--- scourgify.py
+++ scourgify.py
@@ -1,28 +1,24 @@
+import csv
 import sys
-from csv import reader
-from csv import DictWriter
 
-if(sys.argv[1]) != "before.csv":
-    sys.exit("Could not read {sys.argv[1]}")
-elif(sys.argv[2] != "after.csv"):
-    sys.exit("Invalid csv file")
+if len(sys.argv) != 3:
+    sys.exit("Invalid Input")
 
-else:
-    first_name = []
-    last_name = []
-    house_of_them = []
-    with open("before.csv",errors='ignore') as file:
-        read = reader(file)
-        head= next(read)
-        first_name.append("first")
-        last_name.append("last")
-        house_of_them.append("house")
-        for first,last,house in read:
-            first_name.append(first)
-            last_name.append(last)
-            house_of_them.append(house)
+try:
+    student = []
+    with open(sys.argv[1]) as file:
+        read = csv.reader(file)
+        header = next(read)
+        for row in read:
+            row[0].strip('"')
+            frist, last = row[0].split(",")
+            student.append({"frist":frist,"last":last,"house":row[1]})
 
-    with open("after.csv","a") as file:
-        write = DictWriter(file,fieldnames=["first","last","house"])
-        for first2,last2,house2 in zip(first_name,last_name,house_of_them):
-            write.writerow({"first":first2,"last":last2,"house":house2})
+except FileNotFoundError:
+    sys.exit(f"Could not find {sys.argv[1]}")
+
+with open(sys.argv[2],"a",newline='') as file:
+    write = csv.DictWriter(file,fieldnames=["frist","last","house"])
+    write.writeheader()
+    write.writerows(student)
+exit(code=0)
