9fb351d4e3f11bcc25fc924ba0561932bd115e0a
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-11 04:04:30 +0000

    Mon, Jul 10, 2023, 9:04 PM -07:00

--- scourgify.py
+++ scourgify.py
@@ -1,23 +1,36 @@
-import csv
 import sys
+import csv
 
 if len(sys.argv) != 3:
-    sys.exit("Invalid Input")
+    sys.exit("Error: Please provide exactly two command-line arguments.")
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
 
 try:
-    student = []
-    with open(sys.argv[1]) as file:
-        read = csv.reader(file)
-        header = next(read)
-        for row in read:
-            student.append({"frist":row[0].strip('"'),"last":row[1].strip('"'),"house":row[2]})
+    with open(input_file, 'r') as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+        if len(rows) == 0 or len(rows[0]) != 2:
+            sys.exit("Error: Invalid input CSV format. Expected columns: name, house.")
+
+        with open(output_file, 'w', newline='') as output:
+            writer = csv.writer(output)
+            writer.writerow(["first", "last", "house"])
+
+            for row in rows:
+                name = row[0].split()
+                if len(name) != 2:
+                    sys.exit(f"Error: Invalid name format in input CSV. Expected 'first last', got '{row[0]}'.")
+
+                first_name, last_name = name
+                house = row[1]
+                writer.writerow([first_name, last_name, house])
+
+    print(f"Conversion completed. Data written to '{output_file}'.")
+    sys.exit(0)
+
 except FileNotFoundError:
-    sys.exit(f"Could not find {sys.argv[1]}")
+    sys.exit(f"Error: File '{input_file}' not found.")
 except IOError:
     sys.exit("Error: An I/O error occurred while reading or writing the files.")
-
-with open(sys.argv[2],"a",newline='') as file:
-    write = csv.DictWriter(file,fieldnames=["frist","last","house"])
-    write.writeheader()
-    write.writerows(student)
-sys.exit(0)
