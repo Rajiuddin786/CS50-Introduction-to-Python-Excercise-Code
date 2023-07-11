b1eea62cfa1ec2988c02c515cdf1122946b1793f
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-11 03:52:58 +0000

    Mon, Jul 10, 2023, 8:52 PM -07:00

--- scourgify.py
+++ scourgify.py
@@ -10,13 +10,13 @@ try:
         read = csv.reader(file)
         header = next(read)
         for row in read:
-            student.append({"frist":row[0].lstrip('"'),"last":row[1].rstrip('"'),"house":row[2]})
+            student.append({"frist":row[0].strip('"'),"last":row[1].strip('"'),"house":row[2]})
     print(student)
 except FileNotFoundError:
     sys.exit(f"Could not find {sys.argv[1]}")
 
-"""with open(sys.argv[2],"a",newline='') as file:
+with open(sys.argv[2],"a",newline='') as file:
     write = csv.DictWriter(file,fieldnames=["frist","last","house"])
     write.writeheader()
     write.writerows(student)
-exit(code=0)"""
+exit(code=0)
