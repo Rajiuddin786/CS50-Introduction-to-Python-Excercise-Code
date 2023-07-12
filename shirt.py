f3514604b8cf0c92463f7c05ff98574431dbd276
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-12 03:59:09 +0000

    Tue, Jul 11, 2023, 8:59 PM -07:00

---
+++ shirt.py
@@ -0,0 +1,31 @@
+from PIL import Image,ImageOps
+import sys
+
+if len(sys.argv) != 3:
+    sys.exit("Only two command-line arguments")
+
+input_image = sys.argv[1]
+output_image = sys.argv[2]
+
+try:
+   x,y = input_image.split('.')
+   a,b = output_image.split('.')
+except ValueError:
+    sys.exit("Ente a Valid Extension")
+
+if y != "jpg" and y != "jpeg" and y != "png" and b != "jpg" and b != "jpeg" and b != "png":
+    sys.exit("Enter a Valid Extension")
+if y != b:
+    sys.exit("Must have same extension")
+
+try:
+    shirt = Image.open(input_image)
+    size = shirt.size
+
+    with Image.open("shirt.png") as pic:
+        photo = pic.resize(size)
+        shirt.paste(photo,photo)
+        shirt.save(output_image)
+
+except FileNotFoundError:
+    sys.exit(f"File does not exit")
