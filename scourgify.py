44ca97e938d09d8a44e66ce589c978cb49bafb79
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-15 05:49:04 +0000

    Fri, Jul 14, 2023, 10:49 PM -07:00

--- scourgify.py
+++ scourgify.py
@@ -1,24 +1,15 @@
-import csv
 import sys
+import csv
 
-if len(sys.argv) != 3:
-    sys.exit("Invalid Input")
-s = ''
-s1 = ''
-try:
-    student = []
-    with open(sys.argv[1]) as file:
-        read = csv.reader(file)
-        header = next(read)
-        for row in read:
-            student.append({"frist":row[0],"last":row[1],"house":row[2]})
-except FileNotFoundError:
-    sys.exit(f"Could not find {sys.argv[1]}")
-except IOError:
-    sys.exit("Error: An I/O error occurred while reading or writing the files.")
+if len(sys.argv) > 3:
+    sys.exit("Too Many Argument")
+elif len(sys.argv) < 3:
+    sys.exit("Too Few Argument")
+else:
+    _,b = sys.argv[1].split('.')
+    _,y = sys.argv[2].split('.')
+    if b != "csv" and y!= "csv":
+        sys.exit("Not a CSV file")
 
-with open(sys.argv[2],"a",newline='') as file:
-    write = csv.DictWriter(file,fieldnames=["frist","last","house"])
-    write.writeheader()
-    write.writerows(student)
-sys.exit(0)
+def main():
+    
