b87606de612f73d5e3eee93a02e029683e10bde0
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-20 04:27:41 +0000

    Wed, Jul 19, 2023, 9:27 PM -07:00

--- test_working.py
+++ test_working.py
@@ -3,4 +3,6 @@ from working import convert
 def test_convert():
     assert validate("9 AM to 10 PM") == "9:00 to 20:00"
 
-def 
+def test_value_error():
+    with pytest.raise(ValueError):
+        convert("9AM - 10PM")
