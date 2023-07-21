04e48b6b0d92f0b5fcdcec24d06ddd03143cabf7
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-21 04:21:09 +0000

    Thu, Jul 20, 2023, 9:21 PM -07:00

--- test_working.py
+++ test_working.py
@@ -3,6 +3,7 @@ from working import convert
 
 def test_convert():
     assert validate("9 AM to 10 PM") == "9:00 to 20:00"
+    assert validate("12:00 AM to 12:00 PM") == "00:00 to 24:00
 
 def test_value_error():
     with pytest.raise(ValueError):
--- working.py
+++ working.py
@@ -10,7 +10,7 @@ def check_value_error(s):
 
 def convert(s):
     check_value_error(s)
-    check = re.search('^([0-9]{1,2}):?([0-9]{1,2})?\s?(AM|PM)\s?to\s?([0-9]{1,2}):?([0-9]{1,2})?\s?(AM|PM)$',s,re.IGNORECASE)
+    check = re.search('^([0-9]{1,2}):?([0-9]{1,2})?\s(AM|PM)\s?to\s?([0-9]{1,2}):?([0-9]{1,2})?\s(AM|PM)$',s,re.IGNORECASE)
     if check:
         hour_first = int(check.group(1))
         if check.group(2):
@@ -24,20 +24,20 @@ def convert(s):
         else:
             minute_second = 0
         meridian2 = check.group(6)
-        if 0 <= hour_first <= 12 and 0 <= int(minute_first) <= 60:
+        if 0 <= hour_first <= 12 and 0 <= int(minute_first) < 60:
             if not(meridian1 == 'AM' or meridian1 == 'am'):
-                if convert_hour1 != 12:
+                if hour_first != 12:
                     convert_hour1 = hour_first + 12
                 else:
                     convert_hour1 = 12
             else:
-                if convert_hour1 != 12:
+                if hour_first != 12:
                     convert_hour1 = hour_first
                 else:
                     convert_hour1 = 0
         else:
             raise ValueError("Enter Correct Time")
-        if 0 <= hour_second <= 12 and 0 <= int(minute_second) <= 60:
+        if 0 <= hour_second <= 12 and 0 <= int(minute_second) < 60:
             if not(meridian2 == 'AM' or meridian2 == 'am'):
                 if hour_second != 12:
                     convert_hour2 = hour_second + 12
@@ -50,7 +50,6 @@ def convert(s):
                     convert_hour2 = 0
         else:
             raise ValueError('Enter correct time')
-
         if convert_hour1 < 10:
             convert_hour_1 = '0'+str(convert_hour1)
         else:
@@ -69,10 +68,8 @@ def convert(s):
             minute_second_str = str(minute_second)
         return_value = convert_hour_1+":"+minute_first_str+" to "+convert_hour_2+":"+minute_second_str
         return(return_value)
-
-
-
-
+    else:
+        raise ValueError
 
 
 if __name__ == "__main__":
