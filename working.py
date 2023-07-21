fd0f8d872236049322c8927bc00dc47e0a27c3b8
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-21 03:47:55 +0000

    Thu, Jul 20, 2023, 8:47 PM -07:00

--- working.py
+++ working.py
@@ -4,47 +4,59 @@ import sys
 def main():
     print(convert(input('Hours: ')))
 
+def check_value_error(s):
+    if not("to" in s):
+        raise ValueError('Enter VAlid TIme')
+
 def convert(s):
-    if 'to' in s:
-        check = re.search('^([0-9]{1,2}):?([0-9]{1,2})?\s?(AM|PM)\s?to\s?([0-9]{1,2}):?([0-9]{1,2})?\s?(AM|PM)$',s,re.IGNORECASE)
-        if check:
-            hour_first = int(check.group(1))
-            if check.group(2):
-                minute_first = int(check.group(2))
-            else:
-                minute_first = '00'
-            meridian1 = check.group(3)
-            hour_second = int(check.group(4))
-            if check.group(5):
-                minute_second = int(check.group(5))
-            else:
-                minute_second = '00'
-            meridian2 = check.group(6)
-            if 0 <= hour_first <= 12 and 0 <= int(minute_first) <= 60:
-                if not(meridian1 == 'AM' or meridian1 == 'am'):
-                    convert_hour1 = hour_first + 12
-                    convert_hour_1 = str(convert_hour1)
-                else:
-                    convert_hour_1 = '0'+str(hour_first)
-            else:
-                raise ValueError("Enter Correct Time")
-            if 0 <= hour_second <= 12 and 0 <= int(minute_second) <= 60:
-                if not(meridian2 == 'AM' or meridian2 == 'am'):
-                    convert_hour2 = hour_second + 12
-                    convert_hour_2 = str(convert_hour2)
-                else:
-                   convert_hour_2 = '0'+str(hour_second)
-
-            else:
-                raise ValueError('Enter correct time')
-            return_value = convert_hour_1+":"+str(minute_first)+" to "+convert_hour_2+":"+str(minute_second)
-            return(return_value)
+    check_value_error(s)
+    check = re.search('^([0-9]{1,2}):?([0-9]{1,2})?\s?(AM|PM)\s?to\s?([0-9]{1,2}):?([0-9]{1,2})?\s?(AM|PM)$',s,re.IGNORECASE)
+    if check:
+        hour_first = int(check.group(1))
+        if check.group(2):
+            minute_first = int(check.group(2))
         else:
-            return('')
-    else:
-        raise ValueError('Enter Valid Time')
+            minute_first = 0
+        meridian1 = check.group(3)
+        hour_second = int(check.group(4))
+        if check.group(5):
+            minute_second = int(check.group(5))
+        else:
+            minute_second = 0
+        meridian2 = check.group(6)
+        if 0 <= hour_first <= 12 and 0 <= int(minute_first) <= 60:
+            if not(meridian1 == 'AM' or meridian1 == 'am'):
+                convert_hour1 = hour_first + 12
+            else:
+                convert_hour1 = hour_first
+        else:
+            raise ValueError("Enter Correct Time")
+        if 0 <= hour_second <= 12 and 0 <= int(minute_second) <= 60:
+            if not(meridian2 == 'AM' or meridian2 == 'am'):
+                convert_hour2 = hour_second + 12
+            else:
+                convert_hour2 = hour_second
+        else:
+            raise ValueError('Enter correct time')
 
-
+        if convert_hour1 < 10:
+            convert_hour_1 = '0'+str(convert_hour1)
+        else:
+            convert_hour_1 = str(convert_hour1)
+        if minute_first < 10:
+            minute_first_str = '0'+str(minute_first)
+        else:
+            minute_first_str = str(minute_first)
+        if convert_hour2 < 10:
+            convert_hour_2 = '0'+str(convert_hour2)
+        else:
+            convert_hour_2 = str(convert_hour2)
+        if minute_second < 10:
+            minute_second_str = '0'+str(minute_second)
+        else:
+            minute_second_str = str(minute_second)
+        return_value = convert_hour_1+":"+minute_first_str+" to "+convert_hour_2+":"+minute_second_str
+        return(return_value)
 
 
 
