ded9659c67f4be1b034389e4b654b16ba09e4802
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-07-23 03:45:27 +0000

    Sat, Jul 22, 2023, 8:45 PM -07:00

---
+++ um.py
@@ -0,0 +1,15 @@
+import re
+import sys
+
+def main():
+    print(count(input('Text: ')))
+
+def count(s):
+    count = 0
+    count_um = re.findall('um',s,re.IGNORECASE)
+    for _ in count_um:
+        count+=1
+    return count
+
+if __name__ == "__main__":
+    main()
