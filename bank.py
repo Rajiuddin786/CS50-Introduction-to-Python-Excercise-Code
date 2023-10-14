f026b28036b27926f88f5cf347527c2c6862c0b9
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-10-14 03:49:29 +0000

    Fri, Oct 13, 2023, 8:49 PM -07:00

--- bank.py
+++ bank.py
@@ -1,16 +1,41 @@
 def main():
-    greet = (input("Greeting: ")).lower().strip()
-    print(f"${value(greet)}")
+    hello = input("Greeting: ").strip().title()
+    print(f"{check(hello)}")
 
-
-def value(greeting):
-    if greeting.startswith("hello"):
-        return 0
-    elif greeting.startswith("h"):
-        return 20
+def value(hello):
+    my_list = list(hello)
+    lenght = len(my_list)
+    r = 0
+    h = 0
+    for i in range(lenght):
+        if i == 0:
+            if my_list[0] == 'H':
+                r += 1
+                h += 1
+        elif i == 1:
+            if my_list[1] == 'e':
+                r += 1
+                h += 1
+        elif i == 2:
+            if my_list[2] == 'l':
+                r += 1
+                h += 1
+        elif i == 3:
+            if my_list[3] == 'l':
+                r += 1
+                h += 1
+        elif i == 4:
+            if my_list[4] == 'o':
+                r += 1
+                h += 1
+        if i > 5:
+            break
+    if r == 5:
+        return f"$0"
+    if r != 5 and h==1:
+        return f"$20"
     else:
-        return 100
-
+        return f"$0"
 
 if __name__ == "__main__":
     main()
