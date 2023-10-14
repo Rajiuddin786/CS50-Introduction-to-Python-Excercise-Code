5425462d1799ba4dd1adf0d47d8724d0df693c94
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-10-14 04:02:20 +0000

    Fri, Oct 13, 2023, 9:02 PM -07:00

--- test_plates.py
+++ test_plates.py
@@ -3,7 +3,7 @@ from plates import is_valid
 def test_plates():
     assert is_valid("resj12")==True
     assert is_valid("asdfr12334")==False
-    assert is_valid("QWER123")==False
+    assert is_valid("Q123")==False
     assert is_valid("qw1er2")==False
     assert is_valid("QWE0")==False
     assert is_valid("9qwert")==False
