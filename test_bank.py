bab709222e29596503bd7d0e82005e428bbc53e7
Rajiuddin786 <125189764+Rajiuddin786@users.noreply.github.com>
2023-10-14 03:40:11 +0000

    Fri, Oct 13, 2023, 8:40 PM -07:00

--- test_bank.py
+++ test_bank.py
@@ -2,9 +2,9 @@ import pytest
 from bank import check
 
 def test_check():
-    assert check("Hello")=="$0"
-    assert check("Hi")=="$20"
-    assert check("Raj")=="$100"
+    assert check("Hello World")=="$0"
+    assert check("Hi,Raj")=="$20"
+    assert check("Raj is Good")=="$100"
     assert check("HI WORLD") == "$20"
     assert check("wassup world") == "$100"
     assert check("WASSUP WORLD") == "$10"
